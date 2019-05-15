874637043a51d930e74ac2eed5d0a44a8b66136b
Clement <clement.sabourin@hetic.net>
2019-05-15 15:25:55 +0200

    EJS Init

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 /*
 Importer les composants serveur
 */
+    // NodeJS
+    require('dotenv').config();
     const express = require('express');
     const path = require('path');
+
+    // Inner
+    const frontRouter = require('./routes/front.router');
 //
 
 /*
@@ -10,15 +15,21 @@ Configuration du serveur
 */
     // Définir les variables serveur
     const server = express();
-    const port = 3000;
+    const port = process.env.PORT;
+
+    // Configuration du moteur de rendu
+    server.set('view engine', 'ejs'); 
 
     // Définition du dossier static du client
     server.set( 'views', __dirname + '/www' );
     server.use( express.static(path.join(__dirname, 'www')) );
+
+    // Utilisation des routers
+    server.use('/', frontRouter);
 //
 
 /* 
 Lancer le serveur
 */
     server.listen( port, () => console.log(`Server listening on port ${port}`) )
-//©
+//
